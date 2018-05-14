ed892d9e4620a11e4bc5528d91e3e840948e194b
Tormod Landet <tormod@landet.net>
2018-05-14 15:04:00 +0200

    Plot sevaral wave models at a time

--- demos/plot_wave.py
+++ demos/plot_wave.py
@@ -1,51 +1,50 @@
 import numpy
 from matplotlib import pyplot
-from raschii import get_wave_model, WAVE_MODELS, check_breaking_criteria
+from raschii import get_wave_model, check_breaking_criteria
 
 
-def plot_wave(wave_type, height, depth, length, N, linear):
+def plot_wave(wave_type, height, depth, length, N):
     """
     Plot the wave with the given parameters
     """
-    WaveClass = get_wave_model(wave_type)
-    args = dict(height=height, depth=depth, length=length)
-    if 'N' in WaveClass.required_input:
-        args['N'] = N
-    wave = WaveClass(**args)
-    
-    # Vertical axis limits
-    ymax = depth + height
-    ymin = max(depth - length * 2, 0)    # Include only 2 wave lengths dept,
-    ymin = max(depth - height *10, ymin) # but no more than 10 wave heights
-    
-    # Get elevation
-    x = numpy.linspace(0, length/2, 200)
-    eta = wave.surface_elevation(x)
-    
-    # Get velocity
-    X, Y = numpy.meshgrid(numpy.linspace(x[0], x[-1], 11),
-                          numpy.linspace(ymin, eta.max(), 11))
-    vel = wave.velocity(X.ravel(), Y.ravel())
-    U = vel[:,0].reshape(X.shape)
-    V = vel[:,1].reshape(X.shape)
-    # mag_max = (vel[:,0]**2 + vel[:,1]**2)**0.5
-    
     fig = pyplot.figure()
     ax = fig.add_subplot(111)
     
-    # Plot the linear surface elevation
-    if linear:
-        eta_lin = depth + height/2 * numpy.cos(2 * numpy.pi * x / length)
-        ax.plot(x, eta_lin, 'g:')
+    model_names = wave_type.split(',')
+    for model_name in model_names:
+        WaveClass = get_wave_model(model_name)
+        args = dict(height=height, depth=depth, length=length)
+        if 'N' in WaveClass.required_input:
+            args['N'] = N
+        wave = WaveClass(**args)
+        
+        # Vertical axis limits
+        ymax = depth + height
+        ymin = max(depth - length * 2, 0)    # Include only 2 wave lengths dept,
+        ymin = max(depth - height *10, ymin) # but no more than 10 wave heights
+        
+        # Get elevation
+        x = numpy.linspace(0, length/2, 200)
+        eta = wave.surface_elevation(x)
+        
+        # Get velocity
+        X, Y = numpy.meshgrid(numpy.linspace(x[0], x[-1], 11),
+                              numpy.linspace(ymin, eta.max(), 11))
+        vel = wave.velocity(X.ravel(), Y.ravel())
+        U = vel[:,0].reshape(X.shape)
+        V = vel[:,1].reshape(X.shape)
+        
+        # Plot surface elevation
+        ax.plot(x, eta)
+        
+        # Plot colocation points
+        if hasattr(wave, 'eta'):
+            ax.plot(wave.x, wave.eta, 'kx', ms=2)
+        
+        # Plot the velocity components
+        ax.quiver(X, Y, U, V)
     
-    # Plot surface elevation (with colocation points)
-    ax.plot(x, eta)
-    ax.plot(wave.x, wave.eta, 'kx', ms=2)
     ax.set_ylim(ymin, ymax)
-    
-    # Plot the velocity components
-    ax.quiver(X, Y, U, V)
-    
     pyplot.show()
 
 
@@ -54,14 +53,11 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog='raschii_plot_wave',
                                      description='Plot a wave')
-    parser.add_argument('wave_type', help='Name of the wave model',
-                        choices=WAVE_MODELS.keys())
+    parser.add_argument('wave_type', help='Name of the wave model')
     parser.add_argument('wave_height', help='Wave height', type=float)
     parser.add_argument('water_depth', help='The still water depth', type=float)
     parser.add_argument('wave_length', help='Distance between peaks', type=float)
     parser.add_argument('-N', type=int, default=10, help='Approximation order')
-    parser.add_argument('-l', '--linear', default=False, action='store_true',
-                        help='Plot the linear wave elevation')
     parser.add_argument('-f', '--force', default=False, action='store_true',
                         help='Allow exceeding breaking criteria')
     args = parser.parse_args()
@@ -74,4 +70,4 @@ if __name__ == '__main__':
         exit(1)
     
     plot_wave(args.wave_type, args.wave_height, args.water_depth,
-              args.wave_length, args.N, args.linear)
+              args.wave_length, args.N)
--- raschii/airy.py
+++ raschii/airy.py
@@ -1,4 +1,4 @@
-from numpy import pi, cos, sin, zeros, array, asarray
+from numpy import pi, cos, sin, zeros, array, asarray, sinh, cosh, tanh
 
 
 class AiryWave:
@@ -16,6 +16,9 @@ class AiryWave:
         self.depth = depth
         self.length = length
         self.g = g
+        self.k = 2 * pi / length
+        self.omega = (self.k * g * tanh(self.k * depth))**0.5
+        self.c = self.omega / self.k
     
     def surface_elevation(self, x, t=0):
         """
@@ -24,16 +27,33 @@ class AiryWave:
         if isinstance(x, (float, int)):
             x = array([x], float)
         x = asarray(x)
-        
-        return self.depth + self.height / 2 * cos(2 * pi * x / self.length)
+        x = x + self.c * t
+        return self.depth + self.height / 2 * cos(self.k * x)
     
     def velocity(self, x, z, t=0):
         """
         Compute the fluid velocity at time t for position(s) (x, z)
         """
-        pass
+        if isinstance(x, (float, int)):
+            x, z = [x], [z]
+        x = asarray(x, dtype=float)
+        z = asarray(z, dtype=float)
+        
+        H = self.height
+        c = self.c
+        k = self.k
+        d = self.depth
+        w = self.omega
+        x = x + c * t
+        
+        vel = zeros((x.size, 2), float) + 1
+        vel[:, 0] = w * H / 2 * cosh(k * (z + d)) / sinh(k * d) * cos(k * x)
+        vel[:, 1] = w * H / 2 * sinh(k * (z + d)) / sinh(k * d) * sin(k * x)
+        zmax = self.surface_elevation(x, t)
+        vel[z > zmax] = 0
+        
+        return vel
     
     def elevation_cpp(self):
-        return '%r + %r / 2.0 * cos(2 * pi * x[0] / %r)' % (self.depth,
-                                                            self.height,
-                                                            self.length)
+        return '%r + %r / 2.0 * cos(%r * (x[0] + %r * t))' % \
+            (self.depth, self.height, self.k, self.c)
