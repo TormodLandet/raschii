82cdec098b340d16351f0c1f39ee7daf9247838b
Tormod Landet <tormod@landet.net>
2018-05-15 09:21:48 +0200

    Scale velocities in the command line plotting program

--- demos/plot_wave.py
+++ demos/plot_wave.py
@@ -1,16 +1,20 @@
 import numpy
+from math import pi, sinh, cosh, tanh
 from matplotlib import pyplot
 from raschii import get_wave_model, check_breaking_criteria
 
 
-def plot_wave(wave_type, height, depth, length, N):
+def plot_wave(model_names, height, depth, length, N):
     """
-    Plot the wave with the given parameters
+    Plot waves with the given parameters
     """
     fig = pyplot.figure()
     ax = fig.add_subplot(111)
     
-    model_names = wave_type.split(',')
+    head_format = '%15s  %10s %10s %10s %10s'
+    info_format = '%15s  %10.3e %10.3e %10.3e %10.3e'
+    print(head_format % ('', 'c', 'eta_max', 'eta_min', 'vel_max'))
+    
     for model_name in model_names:
         WaveClass = get_wave_model(model_name)
         args = dict(height=height, depth=depth, length=length)
@@ -19,9 +23,9 @@ def plot_wave(wave_type, height, depth, length, N):
         wave = WaveClass(**args)
         
         # Vertical axis limits
-        ymax = depth + height
+        ymax = depth + height * 1.5
         ymin = max(depth - length * 2, 0)    # Include only 2 wave lengths dept,
-        ymin = max(depth - height *10, ymin) # but no more than 10 wave heights
+        ymin = max(depth - height * 10, ymin) # but no more than 10 wave heights
         
         # Get elevation
         x = numpy.linspace(0, length/2, 200)
@@ -34,15 +38,36 @@ def plot_wave(wave_type, height, depth, length, N):
         U = vel[:,0].reshape(X.shape)
         V = vel[:,1].reshape(X.shape)
         
+        # Crest velocity scale (Airy value)
+        k_scale = 2 * pi / length
+        g = 9.81
+        w_scale = (k_scale * g * tanh(k_scale * depth))**0.5
+        Uscale = w_scale * height / 2 * (cosh(k_scale * (depth + height / 2)) /
+                                         sinh(k_scale * depth))
+        
         # Plot surface elevation
-        ax.plot(x, eta)
+        eta_line, = ax.plot(x, eta, label=model_name)
+        eta_color = eta_line.get_color()
         
         # Plot colocation points
         if hasattr(wave, 'eta'):
             ax.plot(wave.x, wave.eta, 'kx', ms=2)
         
         # Plot the velocity components
-        ax.quiver(X, Y, U, V)
+        ax.quiver(X, Y, U, V, scale_units='height', scale=Uscale*8,
+                  color=eta_color, width=3.5e-3, label=None)
+        
+        # Print some info
+        vel_max = ((vel[:,0]**2 + vel[:,1]**2)**0.5).max()
+        print(info_format % (model_name, wave.c, eta.max(), eta.min(), vel_max))
+    
+    print(head_format % ('scale', '', '', '', '%10.3e' % Uscale))
+    
+    if len(model_names) > 1:    
+        ax.legend(loc='lower right')
+        ax.set_title('Wave models %s' % ' and '.join(model_names))
+    else:
+        ax.set_title('Wave model %s' % model_name)
     
     ax.set_ylim(ymin, ymax)
     pyplot.show()
@@ -69,5 +94,6 @@ if __name__ == '__main__':
     if err and not args.force:
         exit(1)
     
-    plot_wave(args.wave_type, args.wave_height, args.water_depth,
+    model_names = args.wave_type.split(',')
+    plot_wave(model_names, args.wave_height, args.water_depth,
               args.wave_length, args.N)
