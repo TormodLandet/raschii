7c1b40972e6ee6c46375f4e4eee3af41f7251ce9
Tormod Landet <tormod@landet.net>
2018-05-24 17:43:56 +0200

    Stokes is missing the C++ velocities for now

--- raschii/stokes.py
+++ raschii/stokes.py
@@ -1,7 +1,7 @@
 from math import pi, sinh, tanh, exp, sqrt, pow
 import numpy
-from .air_phase import (StreamFunctionAirPhase, blend_air_and_wave_velocities,
-                        blend_air_and_wave_velocity_cpp)
+from .air_phase import StreamFunctionAirPhase, blend_air_and_wave_velocities
+
 
 class StokesWave:
     required_input = {'height', 'depth', 'length', 'N'}
